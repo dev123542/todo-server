fcb09ef23724ca784131f16ce986ada1f3bb7870
dev123542 <zappydep@gmail.com>
2021-06-11 00:21:40 +0900

    test code practice

---
+++ src/test/java/org/example/controller/TodoControllerTest.java
@@ -0,0 +1,42 @@
+package org.example.controller;
+
+import org.example.model.TodoEntity;
+import org.example.service.TodoService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(TodoController.class)
+class TodoControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    TodoService todoService;
+
+    private TodoEntity expected;
+
+    @BeforeEach
+    void setup(){
+        // 각 테스트의 메소드가 실행되기 전마다 expected의 값을 초기화
+        this.expected = new TodoEntity();
+        this.expected.setId(123L);
+        this.expected.setTitle("TEST TITLE");
+        this.expected.setOrder(0L);
+        this.expected.setCompleted(false);
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void readOne() {
+    }
+}
